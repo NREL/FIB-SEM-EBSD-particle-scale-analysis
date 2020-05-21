b48665b60e52db1afcf67355cc01ecab695de87f
Alexander Quinn <alex_quinn2@yahoo.com>
2020-05-21 00:58:50 -0400

    Nearly done with Figure 8

--- src/EBSD_visualization_gmisorientation.m
+++ src/EBSD_visualization_gmisorientation.m
@@ -16,7 +16,8 @@ gp_grains = grain_props;
 load('2020-05-20-16-45-47_e03_cln3_entire_ptc_ebsd_seg.mat'); close all;
 gp_ptc = grain_props;
 
-% most frequency orientation adopted - already created and works
+% most frequency orientation adopted - already created and works, move to
+% SI stuffs
 load('2020-01-12-15-03-16_EBSD_03_mfv.mat');
 gp_mfv = grain_props; close all;
 
@@ -29,19 +30,43 @@ gp_mfv = grain_props; close all;
 ebsd_img = imread('DF-NMC-CF-01-e_03.tif');
 
 %% Color maps for impact of cleaning
-f1 = figure; imshow(function_view_speckle_removed(gp_grains)); 
+f1 = figure; imshow(function_view_speckle_removed(gp_grains));
+fig_set();
+
 f2 = figure; imshow(function_view_speckle_removed(gp_ptc)); 
+fig_set();
 
 %% Red-white intragrain misorientations
 f3 = figure; imshow(function_view_intragrain_misorientation(gp_grains)); 
+fig_set();
 
-%% Visualizations: Intra-grain angles per boundary pixel
-f4 = figure; 
-plot_igba(gp_grains)
-f4.Position(2) = 4;
+%% Image Quality histograms
+ptc_map = (gp_ptc.ptc_map > 0);
+grain_map = (gp_grains.ptc_map > 0);
+boundary_map = ptc_map & ~grain_map;
 
+% image_quality, combine
+f8 = figure;
+iq_histos(img_quality2(ptc_map));
+iq_histos(img_quality2(grain_map));
+iq_histos(img_quality2(boundary_map));
+
+% Separate
+f9 = figure; 
+h1 = iq_histos(img_quality2(ptc_map)); h1.FaceColor = 'black';
+f9.Position(1) = 3;
+
+f10 = figure; 
+h2 = iq_histos(img_quality2(grain_map)); h2.FaceColor = 'black';
+f10.Position(1) = 5;
+
+f11 = figure; 
+h3 = iq_histos(img_quality2(boundary_map)); h3.FaceColor = 'black';
+f11.Position(1) = 7;
+
+%% Intra-grain angles per boundary pixel
 f5 = figure; 
-plot_igba(gp_ptc);
+igba_histos(gp_ptc);
 f5.Position(1) = 4; f5.Position(2) = 4;
 
 f6 = figure; 
@@ -50,91 +75,69 @@ ba_image(gp_grains)
 f7 = figure; 
 ba_image(gp_ptc)
 f7.Position(1) = 4;
-hold on; 
+hold on;
 
+% Mask for grain_map
+f12 = figure; f12.Units = 'inches';
+imshow((grain_map))
+f12.Position = [7,1,2.75,2.25];
 
-%% Grain-grain angles per boundary pixel 
-if ~isempty(gp_mfv.grain_border_angles)
-    f8 = figure; histogram(real(gp_mfv.grain_border_angles(:,5)), 72); xlabel('g-misorientation (degrees)'); ylabel('Frequency')
-    function_show_border_angles(gp_mfv, gp_mfv.grain_border_angles);
-    f8.Color = 'white'; f8.Units = 'inches'; f8.Position(3) = 2.75; f8.Position(4) = 2.25;
-end
-
-%% Image Quality histograms
-ptc_map = (gp_ptc.ptc_map > 0);
-grain_map = (gp_grains.ptc_map > 0);
-webbing_map = ptc_map & ~grain_map;
-
-% image_quality, combine
-fff = figure;
-iq_histos(img_quality2(ptc_map));
-iq_histos(img_quality2(grain_map));
-iq_histos(img_quality2(webbing_map));
-
-% Separate
-fff1 = figure; 
-h1 = iq_histos(img_quality2(ptc_map)); h1.FaceColor = 'black';
-fff1.Position(1) = 3;
-
-fff2 = figure; 
-h2 = iq_histos(img_quality2(grain_map)); h2.FaceColor = 'black';
-fff2.Position(1) = 5;
-
-fff3 = figure; 
-h3 = iq_histos(img_quality2(webbing_map)); h3.FaceColor = 'black';
-fff3.Position(1) = 7;
-
-%% Mask for grain_map
-ffff = figure; ffff.Units = 'inches';
-imshow((~grain_map))
-ffff.Position = [7,1,2.75,2.25];
+[angles_b, angles_g] = count_gig_boundaries(gp_ptc, boundary_map);
+ff2 = figure; igba_histos_gen(angles_g); ff2.Position(1) = 1; ff2.Position(2) = 4;
+ff1 = figure; igba_histos_gen(angles_b); ff1.Position(1) = 7; ff1.Position(2) = 4;
 
 %% Intragrain fractions
 [g_sz, ig_f, ig_ft, g_szt] = count_ig_fractions(gp_grains);
 
 % individual intra-grains
-f_ig1 = figure; scatter(g_sz, ig_f, 3, 'filled');
-f_ig1.Color = 'white'; f_ig1.Units = 'inches'; f_ig1.Position(3:4) = [2.75, 2.25];
-xlabel('Grain size (\mum)'); ylabel('f_{individual}')
-a_intg_fran = gca; a_intg_fran.FontSize = 10;
+f13 = figure; scatter(g_sz, ig_f, 3, 'filled');
+fig_set(); xlabel('Grain size (\mum)'); ylabel('f_{total}')
 
 % total fraction of intragrains
-f_ig2 = figure; scatter(g_szt, ig_ft, 8, 'filled');
-f_ig2.Color = 'white'; f_ig2.Units = 'inches'; f_ig2.Position(3:4) = [2.75, 2.25];
-xlabel('Grain size (\mum)'); ylabel('f_{total}')
-f_ig2 = gca; f_ig2.FontSize = 10;
+f14 = figure; scatter(g_szt, ig_ft, 8, 'filled');
+fig_set(); xlabel('Grain size (\mum)'); ylabel('f_{total}')
 
-
-%% Segmentation Map Remove Unsegmented Regions
-img = function_hide_background(grain_props.xyz_cleaned, grain_props.BW);
-figure; imshow(img)
-
-%% Figure Modifications
-fighandles = findobj('Type', 'figure');
-for n = 1:length(fighandles)
-    fighandles(n).Color = 'white';
-    fighandles(n).Units = 'inches';
-    figure(fighandles(n));
-    ax_current = gca;
-    ax_current.FontSize = 10;
-    ax_current.FontName = 'Arial';
+%% Grain-grain angles per boundary pixel - move to SI
+if ~isempty(gp_mfv.grain_border_angles)
+    f100 = figure; histogram(real(gp_mfv.grain_border_angles(:,5)), 72); xlabel('g-misorientation (degrees)'); ylabel('Frequency')
+    function_show_border_angles(gp_mfv, gp_mfv.grain_border_angles);
+    f100.Color = 'white'; f100.Units = 'inches'; f100.Position(3) = 2.75; f100.Position(4) = 2.25;
 end
 
-function plot_igba(M)
-    histogram(M.intragrain_border_angles(:,5), 72)
+%% Plot Help Functions
+function igba_histos(M)
+% intergrain boundary angle histograms
+    igba_histos_gen(M.intragrain_border_angles(:,5))
+end
+
+function igba_histos_gen(M)
+% intergrain boundary angle histograms
+    h = histogram(M, 45); h.EdgeColor = 'none';
     xlabel('g-misorientation (degrees)'); ylabel('Frequency')
     f = gcf; f.Units = 'inches'; f.Position = [1,1,2.75,2.25];
+    f.Color = 'white';
+    ylim([0, 5000])
 end
 
 function ba_image(M)
+% boundary angle maps
     function_show_border_angles(M, M.intragrain_border_angles);
     f = gcf; f.Units = 'inches'; f.Position = [1,1,2.75,2.25];
 end
 
 function h = iq_histos(M)
+% histograms for iq values
     f = gcf; f.Units = 'inches'; f.Position = [1 1 1.9 3];
     h = histogram(M); hold on;
     xlim([6 13]*1e4); ylim([0 12000]);
     xlabel('iq'); ylabel('count')
     h.EdgeColor = 'none';
+end
+
+function fig_set()
+    f = gcf;
+    f.Color = 'white'; 
+    f.Units = 'inches'; 
+    f.Position(3:4) = [2.75, 2.25];
+    f = gca; f.FontSize = 10;
 end
---
+++ src/Visualization/count_gig_boundaries.m
@@ -0,0 +1,24 @@
+function [angles_b, angles_g] = count_gig_boundaries(grain_props, bd_map)
+
+    angles_in_boundary = 0;
+    angles_in_grain = 0;
+    angles_b = nan(length(grain_props.intragrain_border_angles), 1);
+    angles_g = nan(length(grain_props.intragrain_border_angles), 1);
+
+    for n = 1:length(grain_props.intragrain_border_angles)
+        x1 = round(grain_props.intragrain_border_angles(n,1));
+        y1 = round(grain_props.intragrain_border_angles(n,3));
+        angle = grain_props.intragrain_border_angles(n,5);
+
+        if bd_map(x1, y1)
+            angles_in_boundary = angles_in_boundary + 1;
+            angles_b(angles_in_boundary) = angle;
+        else
+            angles_in_grain = angles_in_grain + 1;
+            angles_g(angles_in_grain) = angle;
+        end
+    end
+    angles_b(isnan(angles_b)) = [];
+    angles_g(isnan(angles_g)) = [];
+
+end
