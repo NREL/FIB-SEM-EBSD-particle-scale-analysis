164c59f5f594e10c4cc67eee2e2267854ac8f4d3
Alexander Quinn <alex_quinn2@yahoo.com>
2020-05-21 20:10:41 -0400

    Found correct files for EBSD_visualization_3D_Histogram

--- src/EBSD_visualization_3DHistograms.m
+++ src/EBSD_visualization_3DHistograms.m
@@ -1,31 +1,30 @@
 %% analysis using grain_props
 close all; clear; clc;
 addpath('Visualization')
+addpath('3DHistogram')
 
-%%
-% load grain_props
-% load('2020-01-12-14-20-15_EBSD_03_run1');
-load('2020-05-19-18-42-45_EBSD_03_run1'); close all;
-
-% for slices, and first 2 of figure 7
-%   'e03_updtRandXYZ_cln3_ci05_ebsd_seg_11-Aug-2019 213139.mat'
-% for grains only
-load('e03_cln3_ci05_grains_only_ebsd_seg_18-Jul-2019 104019.mat'); gp_grains = grain_props;
-% for boundary region
-load('e03_webbings_ebsd_seg_17-Jul-2019 101226.mat'); gp_boundaries = grain_props;
-% FULL grain. cleaning applied
-load('e03_cln3_entire_ptc_ebsd_seg_17-Jul-2019 113107.mat'); gp_all = grain_props;
-% most frequency orientation adopted
-%   '2020-01-12-15-03-16_EBSD_03_mfv.mat'
+%% Loading - Originally Used Data in Paper...
+load('e03_cln3_no_ci_grains_only_ebsd_seg_18-Jul-2019 085726.mat'); gp_grains = grain_props;
+load('e03_webbings_ebsd_seg_17-Jul-2019 101226.mat'); gp_boundaries = grain_props; 
+load('e03_cln3_entire_ptc_ebsd_seg_17-Jul-2019 113107.mat'); gp_all = grain_props; 
+load('e03_cln0_ci0_noise_ebsd_seg_22-Jul-2019 164217.mat'); gp_bckgrd = grain_props;
 close all;
 
-%% Orientations by pixel
+%% Orientations by pixel - Original Uncleaned Data
 % directional histogram, pixel-by-pixel
-fig3 = create_3D_hists(gp_all);
-fig1 = create_3D_hists(gp_grains);
-fig2 = create_3D_hists(gp_boundaries);
+fig11 = create_3D_hists(gp_all); view([140,0])
+fig12 = create_3D_hists(gp_grains); view([140,0])
+fig13 = create_3D_hists(gp_boundaries); view([140,0])
+fig14 = create_3D_hists(gp_bckgrd); view([140,0])
 
+fig21 = create_3D_hists(gp_all); view([-40,0])
+fig22 = create_3D_hists(gp_grains); view([-40,0])
+fig23 = create_3D_hists(gp_boundaries); view([-40,0])
+fig24 = create_3D_hists(gp_bckgrd); view([-40,0])
 
+%%
+
+%% Loading - New Set of Data
 
 %% Figure Modifications
 fighandles = findobj('Type', 'figure');
