b539ffe61206ee9efab7c7a12eab9a3a9ba96a01
Alexander Quinn <alex_quinn2@yahoo.com>
2020-05-24 18:54:03 -0400

    Add remaining 3DHistogram files

--- src/Visualization/function_binned_dmap.m
+++ src/Visualization/function_binned_dmap.m
@@ -1,4 +1,4 @@
-function [fig_size_dist_plots, dmap_rad_bounds_fig, results_d2] = function_binned_dmap(grain_props, radial_bins, parameter, background_img)
+function [fig_hist, fig_sections, result] = function_binned_dmap(grain_props, radial_bins, parameter, background_img)
 %function plots radial map on specified image and reports distribution of
 %values between radial groups
 %   [fig_size_dist_plots, dmap_rad_bounds_fig] =
@@ -12,10 +12,10 @@ function [fig_size_dist_plots, dmap_rad_bounds_fig, results_d2] = function_binne
 %           are plotted with radial bins
 % 
 %   Outputs
-%       fig_size_dist_plots - figure handle to probabiliyt distribution
+%       fig_hist - figure handle to probabiliyt distribution
 %           function which uses Function_probability_density written by
 %           Francois Usseglio-Viretta
-%       dmap_rad_bounds_fig - figure handle to radial map
+%       fig_sections - figure handle to radial map
 %
 %   Author: Alexander H Quinn, National Renewable Energy Laboratory (NREL)
 %   Guided/Inspired by: Donal P. Finagan, NREL
@@ -26,20 +26,21 @@ function [fig_size_dist_plots, dmap_rad_bounds_fig, results_d2] = function_binne
     end
 
     edge_dmap = grain_props.ptc_edge_map;
-    pixel_to_um_fctr = grain_props.um_per_pix;
+    pix2um = grain_props.um_per_pix;
+    gbs = grain_props.grain_boundaries;
     
     % radial distribution plot
-    fig_size_dist_plots = figure; fig_size_dist_plots.Color = 'white'; 
-    fig_size_dist_plots.Units = 'inches'; fig_size_dist_plots.Position(3) = 2.5;
-    fig_size_dist_plots.Position(4) = 2.5;
+    fig_hist = figure; fig_hist.Color = 'white'; 
+    fig_hist.Units = 'inches'; fig_hist.Position(3) = 2.5;
+    fig_hist.Position(4) = 2.5;
     
-    dmap_rad_bounds_fig = figure; dmap_rad_bounds_fig.Color = 'white';
-    dmap_rad_bounds_fig.Units = 'inches'; 
+    fig_sections = figure; fig_sections.Color = 'white';
+    fig_sections.Units = 'inches'; 
     
     imshow(function_apply_seg_map_to_img(background_img, grain_props.BW))
-    for n = 1:length(grain_props.grain_boundaries)
+    for n = 1:length(gbs)
         hold on;
-        plot(grain_props.grain_boundaries{n}{1}(:,2), grain_props.grain_boundaries{n}{1}(:,1), 'white', 'LineWidth', 1); 
+        plot(gbs{n}{1}(:,2), gbs{n}{1}(:,1), 'white', 'LineWidth', 1); 
     end
     
     hold on
@@ -49,45 +50,45 @@ function [fig_size_dist_plots, dmap_rad_bounds_fig, results_d2] = function_binne
     radial_bounds = linspace(0, sorted_distances(end), radial_bins+1);
     
     if strcmpi(parameter, 'area')
-        property_of_interest = grain_props.grain_areas.*(pixel_to_um_fctr^2);
-        xlab = 'Grain size (\mum^2)'; 
+        property_of_interest = grain_props.grain_areas.*(pix2um^2);
+        xlab = 'Grain area (\mum^2)'; 
     elseif strcmpi(parameter, 'perimeter')
-        property_of_interest = grain_props.grain_perimeter*pixel_to_um_fctr;
+        property_of_interest = grain_props.grain_perimeter*pix2um;
         xlab = 'Grain perimeter (\mum)'; 
     elseif strcmpi(parameter, 'eccentricity')
         property_of_interest = grain_props.grain_eccentricities;
-        xlab = 'Grain eccentricity'; 
+        xlab = 'Eccentricity'; 
     elseif strcmpi(parameter, 'circularity')
         property_of_interest = grain_props.grain_circularities;
-        xlab = 'Grain circularity';
+        xlab = 'Circularity';
     elseif strcmpi(parameter, 'poa') % perimeter over area
-        property_of_interest = (grain_props.grain_perimeter*pixel_to_um_fctr)./(grain_props.grain_areas.*(pixel_to_um_fctr^2));
-        xlab = 'perimeter/area (\mum^{-1})';
+        property_of_interest = (grain_props.grain_perimeter*pix2um)./(grain_props.grain_areas.*(pix2um^2));
+        xlab = 'Perimeter/area (\mum^{-1})';
     end
     
 
     for n = 1:length(radial_bounds)-1
-        lgd_string{n} = [num2str(radial_bounds(n)*pixel_to_um_fctr, '%.1f'), ' - ' num2str(radial_bounds(n+1)*pixel_to_um_fctr, '%.1f'), ' \mum'];
+        lgd_string{n} = [num2str(radial_bounds(n)*pix2um, '%.1f'), ' - ' num2str(radial_bounds(n+1)*pix2um, '%.1f'), ' \mum'];
         d_rng = [radial_bounds(n), radial_bounds(n+1)]
-        count = 1; collected_areas = [];
+        count = 1; collect_prop = [];
         for m = 1:length(grain_props.grain_centroids) % input (grain_props)
             curr_d = grain_props.grain_dist(m);
             if curr_d > d_rng(1) && curr_d <= d_rng(2)
-                collected_areas(count) = property_of_interest(m);
+                collect_prop(count) = property_of_interest(m);
                 count = count + 1;
             end
         end
-        figure(fig_size_dist_plots); % OUTPUT
-        results_d2 = function_prob_density_function_wrapper(collected_areas);
+        figure(fig_hist); % OUTPUT
+        result = function_prob_density_function_wrapper(collect_prop);
         
-        results_d2.x50
-        
-        if size(results_d2.smoothed_probability_density_fct, 1) ~= 0
-            results_d2.smoothed_probability_density_fct = cat(1, [0,0], results_d2.smoothed_probability_density_fct);
-            plot(results_d2.smoothed_probability_density_fct(:,1), results_d2.smoothed_probability_density_fct(:,2), 'LineWidth', 1.5); hold on; % INPUT (pixel_to_um_fctr)
+        if size(result.smoothed_probability_density_fct, 1) ~= 0
+            result.smoothed_probability_density_fct = cat(1, [0,0], result.smoothed_probability_density_fct);
+            plot(result.smoothed_probability_density_fct(:,1), ...
+                result.smoothed_probability_density_fct(:,2), ...
+                'LineWidth', 1.5); hold on;
         end
         
-        figure(dmap_rad_bounds_fig); % OUTPUT
+        figure(fig_sections); % OUTPUT
         count2 = 1; xss = []; yss = [];
         for k = 1:size(edge_dmap, 1)
             for m = 1:size(edge_dmap, 2)
@@ -102,9 +103,9 @@ function [fig_size_dist_plots, dmap_rad_bounds_fig, results_d2] = function_binne
         scatter(yss, xss, 0.5, 'k.'); hold on;
     end
     
-    dmap_rad_bounds_fig.Position(3) = 2.5; dmap_rad_bounds_fig.Position(4) = 2.5;
+    fig_sections.Position(3) = 2.5; fig_sections.Position(4) = 2.5;
     
-    figure(fig_size_dist_plots);
+    figure(fig_hist);
     xlabel(xlab); ylabel('Normalized frequency')
     ax_freq_dist = gca; ax_freq_dist.FontSize = 10;
     lgd_freq_dist = legend(lgd_string); lgd_freq_dist.Box = 'off';
